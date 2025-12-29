bf3b9c0cba03b07649ffde4ab9ceb468a5db8c8b
Onur Inan <onur-inan05@hotmail.com>
2025-12-29 22:42:26 +0300

    not found page

---
+++ src/components/NotFound.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { useNavigate } from "react-router-dom"
+import "../css/NotFound.css"
+
+const NotFound = () => {
+    const navigate = useNavigate()
+    return (
+        <div className="not-found__content">
+            <h1 className="not-found" onClick={() => navigate("/")}>Page Not Found</h1>
+        </div>
+    )
+}
+
+export default NotFound
--- src/config/RouterConfig.jsx
+++ src/config/RouterConfig.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import { Routes, Route } from "react-router-dom"
 import Home from "../pages/Home"
 import ProductDetails from "../components/ProductDetails"
+import NotFound from "../components/NotFound"
 
 const RouterConfig = () => {
     return (
         <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/product-details/:id" element={<ProductDetails />} />
+            <Route path="*" element={<NotFound />} />
         </Routes>
     )
 }
